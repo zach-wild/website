126159904216d18ffe57e1b54c757a62cb5e8e6d
zach-wild <diggitcraft@gmail.com>
2019-10-14 13:40:58 -0400

    Initial commit

---
+++ client/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.use(express.static('public'))
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
